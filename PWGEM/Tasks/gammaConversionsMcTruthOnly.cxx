306b780fc956bb3380753a901758eb1bb9b501ec
Stephan Stiefelmaier <stiefelmaier@physi.uni-heidelberg.de>
2022-04-21 20:31:43 +0200

    add _p histograms

--- PWGEM/Tasks/gammaConversionsMcTruthOnly.cxx
+++ PWGEM/Tasks/gammaConversionsMcTruthOnly.cxx
@@ -131,44 +131,58 @@ struct gammaConversionsMcTruthOnly {
   HistogramRegistry registry{
     "registry",
     {
+      {"hNDaughters", "hNDaughters", {HistType::kTH1F, {{50, 0.f, 50.f}}}},
       {"hCollisionZ", "hCollisionZ", {HistType::kTH1F, {{800, -10.f, 10.f}}}},
-      {"hGammaProdInAcc", "hGammaProdInAcc", {HistType::kTH1F, {{800, 0.f, 25.f}}}},
+      {"hGammaProdInEtaAccP", "hGammaProdInEtaAccP", {HistType::kTH1F, {{800, 0.f, 25.f}}}},
+      {"hGammaProdInEtaAccPt", "hGammaProdInEtaAccPt", {HistType::kTH1F, {{800, 0.f, 25.f}}}},
       {"hGammaMoreThanTwoDaughtersPt", "hGammaMoreThanTwoDaughtersPt", {HistType::kTH1F, {{800, 0.f, 25.f}}}},
-      {"hGammaConvertedPt", "hGammaConvertedPt", {HistType::kTH1F, {{800, 0.f, 25.f}}}},
+      {"hGammaConvertedRP", "hGammaConvertedRP", {HistType::kTH2F, {{400, 0.f, 250.f}, {400, 0.f, 25.f}}}},
       {"hGammaConvertedR", "hGammaConvertedR", {HistType::kTH1F, {{1600, 0.f, 500.f}}}},
+      {"hGammaConvertedRselP", "hGammaConvertedRselP", {HistType::kTH1F, {{800, 0.f, 25.f}}}},
+      
       {"hGammaConvertedRPt", "hGammaConvertedRPt", {HistType::kTH2F, {{400, 0.f, 250.f}, {400, 0.f, 25.f}}}},
-    },
+      {"hGammaConvertedRselPt", "hGammaConvertedRselPt", {HistType::kTH1F, {{800, 0.f, 25.f}}}}
+      },
   };
 
   // loop over MC truth McCollisions
-  void process(aod::McCollision const& mcCollision,
+  void process(aod::McCollision const& theMcCollision,
                soa::SmallGroups<soa::Join<aod::McCollisionLabels,
-                                          aod::Collisions>> const& collisions,
+                                          aod::Collisions>> const& theCollisions,
                aod::McParticles const& theMcParticles)
   {
-    for (auto& collision : collisions) {
+    for (auto &lCollision : theCollisions) {
 
-      registry.fill(HIST("hCollisionZ"), collision.posZ());
-      for (auto& lMcParticle : theMcParticles) {
+      registry.fill(HIST("hCollisionZ"), lCollision.posZ());
+      for (auto &lMcParticle : theMcParticles) {
 
         if ((lMcParticle.pdgCode() == 22) &&
             (std::abs(lMcParticle.eta()) < 0.8)) { // SFS todo: track v0 eta??
 
-          registry.fill(HIST("hGammaProdInAcc"), lMcParticle.pt());
-
+          registry.fill(HIST("hGammaProdInEtaAccP"), lMcParticle.p());
+          registry.fill(HIST("hGammaProdInEtaAccPt"), lMcParticle.pt());
+          
+          size_t lNDaughters = 0;
           size_t lBothElectrons = 0;
           if (lMcParticle.has_daughters()) {
-            for (auto& lDaughter : lMcParticle.daughters_as<aod::McParticles>()) {
+            for (auto &lDaughter : lMcParticle.daughters_as<aod::McParticles>()) {
+              ++lNDaughters;
               lBothElectrons += std::abs(lDaughter.pdgCode()) == 11;
             }
+            registry.fill(HIST("hNDaughters"), 0.5 + lNDaughters);
             if (lBothElectrons == 2) {
-              for (auto& lDaughter : lMcParticle.daughters_as<aod::McParticles>()) {
+              if (lNDaughters != 2){
+                LOGF(info, "SFS ALARM");
+              }
+              for (auto &lDaughter : lMcParticle.daughters_as<aod::McParticles>()) {
                 float lConversionRadius = std::sqrt(std::pow(lDaughter.vx(), 2) + std::pow(lDaughter.vy(), 2));
                 registry.fill(HIST("hGammaConvertedR"), lConversionRadius);
+                registry.fill(HIST("hGammaConvertedRP"), lConversionRadius, lMcParticle.p());
                 registry.fill(HIST("hGammaConvertedRPt"), lConversionRadius, lMcParticle.pt());
 
                 if (lConversionRadius > 5. && lConversionRadius < 180.) {
-                  registry.fill(HIST("hGammaConvertedPt"), lMcParticle.pt());
+                  registry.fill(HIST("hGammaConvertedRselP"), lMcParticle.p());
+                  registry.fill(HIST("hGammaConvertedRselPt"), lMcParticle.pt());
                 }
                 break;
               }
